89b0ad0a8c4ff97fef5c127e140a608b444200ce
Jibin Mathews <jibinmathews7@gmail.com>
2018-04-13 00:23:30 +0530

    initial commit

---
+++ app/src/main/java/work/idontlike/idontlikework/Constants.java
@@ -0,0 +1,10 @@
+package work.idontlike.idontlikework;
+
+/**
+ * Created by rentomojo on 4/13/18.
+ */
+
+public class Constants {
+  private static final String API_HOST = "https://sheetsu.com/apis/v1.0su/31894cf15305";
+  public static final String SHEETS_URL = API_HOST;
+}
